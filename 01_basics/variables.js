f9df96b441072fca9e26c0a4cb9aa39d90c4936f
mobasshir-07 <109893390+mobasshir-07@users.noreply.github.com>
2024-02-13 15:35:43 +0000

    updated variabl.js file

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 14425
+let accountEmail = "mobasshir@googl.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 // It is not allowed
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])

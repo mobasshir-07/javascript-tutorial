1a816dcc283520dbea4e21427357a0b3d2c5d1bd
mobasshir-07 <109893390+mobasshir-07@users.noreply.github.com>
2024-02-13 16:01:53 +0000

    datatypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all JS code as newer version
+/* considered a best practice for modern JavaScript development as it helps catch errors early and promotes writing cleaner, more maintainable code. */
+console.log(typeof 12);
+console.log(typeof false);
+console.log(typeof "string");  // string 
+console.log(typeof null);      // object
+console.log(typeof undefined); // undefined
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -5,5 +5,9 @@ accountCity = "Jaipur"
 let accountState
 
 // accountId = 2 // It is not allowed
+/* 
+prefer not to use var
+because of issue in block scope and function scope
+*/
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
